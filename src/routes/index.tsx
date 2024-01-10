bfa4a609e9d6eb6de0c0082a993a87c404616c45
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-01-10 07:19:17 +0700

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+    return <>
+        <div className="bg-slate-300 min-h-screen w-full text-white flex justify-center items-center">
+            Trying tailwind
+        </div>
+    </>
+}
