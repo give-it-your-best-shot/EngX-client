1fa8eeb78954c51964a64dabdab314ecdd0338c9
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-01-10 21:17:31 +0700

    Promp Service

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+import AzureOpenAIService from "../services/azure_openai_service"
+
 export default function Index() {
+    const ai = AzureOpenAIService.getInstance()
+    useEffect(() => {
+        const start = async () => {
+            const message1 = await ai.prompt("Hi")
+            console.log(message1)
+            const message2 = await ai.prompt("I'm testing your API")
+            console.log(message2)
+        }
+        start()
+    }, [])
     return <>
         <div className="bg-slate-300 min-h-screen w-full text-white flex justify-center items-center">
             Trying tailwind
