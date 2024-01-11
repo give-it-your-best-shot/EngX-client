292245dbcf0b9e8ead3466687965184028aacf7c
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-01-11 08:53:23 +0700

    fix routers

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,18 @@
-import { useEffect } from "react"
-import AzureOpenAIService from "../services/azure_openai_service"
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 
 export default function Index() {
-    const ai = AzureOpenAIService.getInstance()
-    useEffect(() => {
-        const start = async () => {
-            const message1 = await ai.prompt("Hi")
-            console.log(message1)
-            const message2 = await ai.prompt("I'm testing your API")
-            console.log(message2)
-        }
-        start()
-    }, [])
-    return <>
-        <div className="bg-slate-300 min-h-screen w-full text-white flex justify-center items-center">
-            Trying tailwind
-        </div>
+  useEffect(() => {
+    const start = async () => {
+      
+    }
+    start()
+  }, [])
+  return (
+    <>
+      <div>
+
+      </div>
     </>
+  );
 }
