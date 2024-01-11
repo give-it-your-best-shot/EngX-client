2122d2fd23b02737fa98d52d43b1de519ab4fe6a
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2024-01-11 09:06:51 +0700

    global background

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import "./index.css";
 import Index from "./routes";
 import { NextUIProvider } from "@nextui-org/react";
 import Login from "./routes/login";
-import Signup from "./routes/Signup";
+import Signup from "./routes/signup";
+import Word from "./routes/word";
 // import App from "./App";
 
 const router = createBrowserRouter([
@@ -20,14 +21,19 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />
+  },
+  {
+    path: "/word/:word",
+    element: <Word />
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <RouterProvider router={router} />
-      {/* <App/> */}
+      <div className="flex justify-center items-center w-full h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+        <RouterProvider router={router} />
+      </div>
     </NextUIProvider>
   </React.StrictMode>
 );
---
+++ src/routes/word.tsx
@@ -0,0 +1,5 @@
+export default function Word() {
+    return <div className="bg-white p-8 rounded-lg shadow-lg w-full">
+            
+    </div>
+}
