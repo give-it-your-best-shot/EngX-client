7308ac87625050f58339b8cfd461b116abbd9f3a
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2024-01-12 23:56:47 +0700

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
+import AzureOpenAIService from "../services/azure_openai_service";
 
 export default function Index() {
   useEffect(() => {
-    const start = async () => {};
-    start();
-  }, []);
+    const start = async () => {}
+    start()
+  }, [])
   return (
     <>
       <div></div>
