81320cf2318c7aef4f28528da1e2f2d7c6ee98e4
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2024-01-20 11:45:59 +0700

    build: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  <React.StrictMode></React.StrictMode>
+);
