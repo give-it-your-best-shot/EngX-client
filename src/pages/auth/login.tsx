0b188f79986e67b870232464e40d1ac08248e2cb
ThaiKhacDuoc <khacduockhamduc@gmail.com>
2024-01-22 22:22:47 +0700

    fix: Logo Google

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -8,7 +8,6 @@ import { FormEvent } from "react";
 import AuthService from "src/services/auth_service";
 import { useAuthenticationStore } from "src/stores";
 import { ACCESS_TOKEN_EXPIRE, REFRESH_TOKEN_EXPIRE } from "src/utils/const";
-import GoogleLogin from "react-google-login";
 
 interface LoginProps {
   paragraph?: string;
@@ -19,7 +18,7 @@ interface LoginProps {
 }
 
 export default function Login({
-  paragraph = "Don't have an account yet?",
+  paragraph = "Don't have an account yet? ",
   linkName = "Sign up",
   linkUrl = "/signup",
   titleInput1 = "Email",
@@ -62,6 +61,10 @@ export default function Login({
     setLoginFail(false);
     const value = e.target.value;
     return value;
+  };
+
+  const handleGoogleButtonClick = () => {
+    window.location.href = "http://localhost:8080/oauth2/authorization/google";
   };
 
   return (
@@ -119,17 +122,67 @@ export default function Login({
                   Login
                 </Button>
               </div>
-              <a
-                className="bg-blue-600"
-                href="http://localhost:8080/oauth2/authorization/google"
+              <button
+                onClick={handleGoogleButtonClick}
+                className="flex ml-10 mt-6 items-center bg-white dark:bg-gray-900 border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 dark:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
               >
-                <span className="button-text">Login with Google</span>
-              </a>
-              <GoogleLogin
-                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                onSuccess={() => {}}
-                isSignedIn={true}
-              />
+                <svg
+                  className="h-6 w-6 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="800px"
+                  height="800px"
+                  viewBox="-0.5 0 48 48"
+                  version="1.1"
+                >
+                  {" "}
+                  <title>Google-color</title> <desc>Created with Sketch.</desc>{" "}
+                  <defs> </defs>{" "}
+                  <g id="Icons" stroke="none" fill="none">
+                    {" "}
+                    <g
+                      id="Color-"
+                      transform="translate(-401.000000, -860.000000)"
+                    >
+                      {" "}
+                      <g
+                        id="Google"
+                        transform="translate(401.000000, 860.000000)"
+                      >
+                        {" "}
+                        <path
+                          d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
+                          id="Fill-1"
+                          fill="#FBBC05"
+                        >
+                          {" "}
+                        </path>{" "}
+                        <path
+                          d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
+                          id="Fill-2"
+                          fill="#EB4335"
+                        >
+                          {" "}
+                        </path>{" "}
+                        <path
+                          d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
+                          id="Fill-3"
+                          fill="#34A853"
+                        >
+                          {" "}
+                        </path>{" "}
+                        <path
+                          d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
+                          id="Fill-4"
+                          fill="#4285F4"
+                        >
+                          {" "}
+                        </path>{" "}
+                      </g>{" "}
+                    </g>{" "}
+                  </g>{" "}
+                </svg>
+                <span>Continue with Google</span>
+              </button>
             </form>
           </div>
         </div>
