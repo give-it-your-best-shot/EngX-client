8277854ecf19e8091cc9ef7e378cd5082eef1865
huynh dang <huynhdang@MacBook-Pro-cua-huynh.local>
2024-01-23 11:39:35 +0700

    feat: record api fetch

--- src/services/user_service.tsx
+++ src/services/user_service.tsx
@@ -1,3 +1,6 @@
+import { UserRecord } from "src/types/user_record.type";
+import http from "src/utils/http";
+
 export default class UserService {
   private static instance: UserService;
   public static getInstance(): UserService {
@@ -6,4 +9,22 @@ export default class UserService {
   }
 
   private constructor() {}
+
+  public async getUnitsRecordByUserId(
+    userId: number
+  ): Promise<UserRecord | null> {
+    const response = await http.get(`/records/users/${userId}/units`);
+    const data = response.data;
+    if ("error" in data) return null;
+    return data.payload;
+  }
+
+  public async getBooksRecordByUserId(
+    userId: number
+  ): Promise<UserRecord | null> {
+    const response = await http.get(`/records/users/${userId}/books`);
+    const data = response.data;
+    if ("error" in data) return null;
+    return data["payload"];
+  }
 }
